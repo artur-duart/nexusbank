aac970766cc191a678496621d618ea938cd9a6f0
Artur Duarte <arturduartemoraes@gmail.com>
2023-06-05 15:59:02 -0300

    🚧 Creating TS files

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import { Negotiation } from './models/negotiation.js';
 
-const negotiation = new Negotiation(new Date(), 10, 100);
+const negotiation = new Negotiation(new Date());
 
 console.log(negotiation.date);
